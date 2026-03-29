cfd1dd953dfbd6b9cb943fdd4ad56dfa977a33d8
Rajat Mani Tripathi <91048134+RajatmaniTripathi10@users.noreply.github.com>
2026-03-29 22:24:54 +0530

    Java 8 done today

--- JAVA8/NonRepeat.java
+++ JAVA8/NonRepeat.java
@@ -5,13 +5,23 @@ import java.util.stream.Collectors;
 public class NonRepeat {
     public static void main(String[] args) {
         String s = "aabbcde";
-        List<Character> lst=s.chars()
-                                .mapToObj(c->(char)c)
-                                .collect(Collectors.groupingBy(c->c,LinkedHashMap::new,Collectors.counting()))
-                                .entrySet().stream()
-                                .filter(c->c.getValue()==1)
-                                .map(c->c.getKey())
-                                .collect(Collectors.toList());
-                System.out.println(lst);
+        //Find the first non-repeating character in the string using Stream functions.
+        Character ch=s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(c->c,LinkedHashMap::new,Collectors.counting()))
+            .entrySet().stream()
+            .filter(c->c.getValue()==1)
+            .map(c->c.getKey())
+            .findFirst().orElse(null);
+                    
+            System.out.println("First non-repeating character : "+ch);
+        
+        //Find all non-repeating characters in the string using Stream functions.
+        List<Character> ch1=s.chars().mapToObj(c->(char)c)
+                            .collect(Collectors.groupingBy(c->c,
+                            LinkedHashMap::new,Collectors.counting()))
+                            .entrySet().stream()
+                            .filter(c->c.getValue()==1)
+                            .map(c->c.getKey())
+                            .collect(Collectors.toList());
+                    System.out.println("All non-repeating characters : "+ch1);
     }
 }
--- JAVA8/SortStringsByLength.java
+++ JAVA8/SortStringsByLength.java
@@ -1,18 +1,20 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortStringsByLength {
     public static void main(String[] args) {
        List<String> strings=Arrays.asList("Apple","Date","Banana","Orange");
+       System.out.println(strings);
 
-      //  List<String> sortedStrings=strings.stream()
-      //  .sorted((s1,s2)->Integer.compare(s1.length(), s2.length())).collect(Collectors.toList());
-   String LongestString=strings.stream() .reduce((s1, s2) -> s1.length() > s2.length() ? s1 : s2)
-    .orElse("");
-       
+       List<Integer> length=strings.stream().map(s->s.length()).collect(Collectors.toList());
+        System.out.println("Length of each string : "+length);
+         
+        List<String> sorted=strings.stream().sorted((s1,s2)->s1.length()-s2.length()).collect(Collectors.toList());
+        System.out.println("Strings sorted by length : "+sorted);
 
-      //  System.out.println(sortedStrings);
-      System.out.println(LongestString);
+        List<String> sortedDescending=strings.stream().sorted((s1,s2)->s2.length()-s1.length()).collect(Collectors.toList());
+        System.out.println("Strings sorted by length (descending) : "+sortedDescending);
     }
     
 }
